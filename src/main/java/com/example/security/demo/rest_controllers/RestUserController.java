a26f433e8ca9e95c3ac1514974a9a3319381e950
IWannaWish <138825123+IWannaWish@users.noreply.github.com>
2023-11-11 23:22:18 +0300

    add user rest controller

--- src/main/java/com/example/security/demo/rest_controllers/RestUserController.java
+++ src/main/java/com/example/security/demo/rest_controllers/RestUserController.java
@@ -5,19 +5,26 @@ import com.example.security.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/user/api")
+@RequestMapping("/api/user")
 public class RestUserController {
     private final UserService userService;
 
     @Autowired
     public RestUserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<User> show(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
